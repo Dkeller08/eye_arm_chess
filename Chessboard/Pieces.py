cb073a9de360a9b9a7d22111a5d593d69cb095e9
Dkeller08 <danielkellerjazz@gmail.com>
2021-09-17 17:32:43 +0200

    Update Pieces

--- Chessboard/Pieces.py
+++ Chessboard/Pieces.py
@@ -6,7 +6,8 @@ class Rook:
     def __init__(self, square, player):
         self.square = square
         self.player = player
-        self.image = pygame.image.load("../Images/"+player+"_rook.png")
+        self.image = pygame.image.load("../Images/" + player + "_rook.png")
+        self.image = pygame.transform.scale(self.image, (int(self.screenx / 8), int(self.screeny / 8)))
         for i in range(8):
             for j in range(8):
                 if square.number == i or square.letter == j:
@@ -18,7 +19,45 @@ class Bishop:
         self.square = square
         self.player = player
         self.image = pygame.image.load("../Images/" + player + "_bishop.png")
+        self.image = pygame.transform.scale(self.image, (int(self.screenx / 8), int(self.screeny / 8)))
         for i in range(8):
             for j in range(8):
                 if square.number - square.letter == i - j or square.number + square.letter == i + j:
+                    Window.squares[i][j].possibleMove = True
+
+
+class Horse:
+    def __init__(self, square, player):
+        self.square = square
+        self.player = player
+        self.image = pygame.image.load("../Images/" + player + "_horse.png")
+        self.image = pygame.transform.scale(self.image, (int(self.screenx / 8), int(self.screeny / 8)))
+        for i in range(8):
+            for j in range(8):
+                if (abs(square.number - i) == 1 and abs(square.letter == 2)) or (
+                        abs(square.number - i) == 2 and abs(square.letter == 1)):
+                    Window.squares[i][j].possibleMove = True
+
+
+class King:
+    def __init__(self, square, player):
+        self.square = square
+        self.player = player
+        self.image = pygame.image.load("../Images/" + player + "_king.png")
+        self.image = pygame.transform.scale(self.image, (int(self.screenx / 8), int(self.screeny / 8)))
+        for i in range(8):
+            for j in range(8):
+                if abs(square.number - i) <= 1 and abs(square.letter <= 1):
+                    Window.squares[i][j].possibleMove = True
+
+
+class Queen:
+    def __init__(self, square, player):
+        self.square = square
+        self.player = player
+        self.image = pygame.image.load("../Images/" + player + "_queen.png")
+        self.image = pygame.transform.scale(self.image, (int(self.screenx / 8), int(self.screeny / 8)))
+        for i in range(8):
+            for j in range(8):
+                if square.number == i or square.letter == j or square.number - square.letter == i - j or square.number + square.letter == i + j:
                     Window.squares[i][j].possibleMove = True
