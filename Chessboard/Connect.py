1c17a24a393a5fe4af80c585781e59ce6645efb7
Dkeller08 <danielkellerjazz@gmail.com>
2021-10-12 13:56:54 +0200

    Eyelink connect

--- Chessboard/Connect.py
+++ Chessboard/Connect.py
@@ -8,7 +8,7 @@ import sys
 # the "el_tracker" objected created here can be accessed through the Pylink
 edf_fname = "Chess"
 try:
-    el_tracker = pylink.Eyelink("100.1.1.1")
+    el_tracker = pylink.EyeLink("100.1.1.1")
 except RuntimeError as error:
     print('ERROR:', error)
     pygame.quit()
@@ -25,3 +25,66 @@ except RuntimeError as err:
         el_tracker.close()
     pygame.quit()
     sys.exit()
+
+# Step 3: Configure the tracker
+#
+# Put the tracker in offline mode before we change tracking parameters
+el_tracker.setOfflineMode()
+# Get the software version: 1-EyeLink I, 2-EyeLink II, 3/4-EyeLink 1000,
+# 5-EyeLink 1000 Plus, 6-Portable DUO
+EyeLink_ver = 0 # set version to 0, in case running in Dummy mode
+
+vstr = el_tracker.getTrackerVersionString()
+EyeLink_ver = int(vstr.split()[-1].split('.')[0])
+
+# print out some version info in the shell
+print('Running experiment on %s, version %d' % (vstr, EyeLink_ver))
+# File and Link data control
+# what eye events to save in the EDF file, include everything by default
+file_event_flags = 'LEFT,RIGHT,FIXATION,SACCADE,BLINK,MESSAGE,BUTTON,INPUT'
+# what eye events to make available over the link, include everything by default
+link_event_flags = 'LEFT,RIGHT,FIXATION,SACCADE,BLINK,BUTTON,FIXUPDATE,INPUT'
+# what sample data to save in the EDF data file and to make available
+# over the link, include the 'HTARGET' flag to save head target sticker
+# data for supported eye trackers
+if EyeLink_ver> 3:
+    file_sample_flags = 'LEFT,RIGHT,GAZE,HREF,RAW,AREA,HTARGET,GAZERES,BUTTON,STATUS,INPUT'
+    link_sample_flags = 'LEFT,RIGHT,GAZE,GAZERES,AREA,HTARGET,STATUS,INPUT'
+else:
+    file_sample_flags = 'LEFT,RIGHT,GAZE,HREF,RAW,AREA,GAZERES,BUTTON,STATUS,INPUT'
+    link_sample_flags = 'LEFT,RIGHT,GAZE,GAZERES,AREA,STATUS,INPUT'
+el_tracker.sendCommand("file_event_filter = %s" % file_event_flags)
+el_tracker.sendCommand("file_sample_data = %s" % file_sample_flags)
+el_tracker.sendCommand("link_event_filter = %s" % link_event_flags)
+el_tracker.sendCommand("link_sample_data = %s" % link_sample_flags)
+
+# Choose a calibration type, H3, HV3, HV5, HV13 (HV = horizontal/vertical),
+el_tracker.sendCommand("calibration_type = HV9")
+# Set a gamepad button to accept calibration/drift check target
+# You need a supported gamepad/button box that is connected to the Host PC
+el_tracker.sendCommand("button_function 5 'accept_target_fixation'")
+
+
+# Pass the display pixel coordinates (left, top, right, bottom) to the tracker
+# see the EyeLink Installation Guide, "Customizing Screen Settings"
+el_coords = "screen_pixel_coords = 0 0 %d %d" % (1920, 1080)
+el_tracker.sendCommand(el_coords)
+
+# Write a DISPLAY_COORDS message to the EDF file
+# Data Viewer needs this piece of info for proper visualization, see Data
+# Viewer User Manual, "Protocol for EyeLink Data to Viewer Integration"
+dv_coords = "DISPLAY_COORDS 0 0 %d %d" % (scn_width - 1, scn_height - 1)
+el_tracker.sendMessage(dv_coords)
+
+task_msg = 'In the task, you may press the SPACEBAR to end a trial\n' + \
+'\nPress Ctrl-C to if you need to quit the task early\n'
+
+task_msg = task_msg + '\nNow, press ENTER to start the task'
+
+# skip this step if running the script in Dummy Mode
+
+try:
+    el_tracker.doTrackerSetup()
+except RuntimeError as err:
+    print('ERROR:', err)
+    el_tracker.exitCalibration()
