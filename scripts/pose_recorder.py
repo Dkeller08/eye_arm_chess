33a0c0dc8bf4ed1b26993e7c6086579c5a583c80
Dkeller08 <danielkellerjazz@gmail.com>
2022-02-11 11:26:45 +0100

    Able to record poses

---
+++ scripts/pose_recorder.py
@@ -0,0 +1,19 @@
+from __future__ import print_function
+from six.moves import input
+
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+from std_msgs.msg import String
+from moveit_commander.conversions import pose_to_list
+
+moveit_commander.roscpp_initialize(sys.argv)
+rospy.init_node("move_arm", anonymous=True)
+robot = moveit_commander.RobotCommander()
+scene = moveit_commander.PlanningSceneInterface()
+group_name = "panda_arm"
+move_group = moveit_commander.MoveGroupCommander(group_name)
+rospy.loginfo(move_group.get_current_pose())
